d3021e270658a315b96ff338a6e8e8a804709df9
j138 <j138cm@gmail.com>
2014-05-16 20:13:40 +0900

    スクリプト追記、レイアウト組で仮で書いた

--- app/views/annotate.blade.php
+++ app/views/annotate.blade.php
@@ -1,5 +1,30 @@
 @extends('layout/master')
 
+@section('script')
+<link rel="stylesheet" type="text/css" href="crop/crop.css" />
+@stop
+
 @section('content')
-<p>こんてんつう</p>
+ねこねこ<br />
+画像を表示し、囲んだ領域をうにょうにょで表示するためのデモ
+<div class="crop-holder">
+	<img class="target-img" src="crop/cat01.jpg" />
+
+	<!-- 囲みたい領域をwidth, heightで指定 -->
+	<div class="crop-wrapper" style="left:40px;top:20px; width:140px; height:130px">
+		<div class="crop-vline"></div>
+		<div class="crop-vline right"></div>
+		<div class="crop-hline"></div>
+		<div class="crop-hline bottom"></div>
+	</div>
+
+	<!-- 2個めも可能 -->
+	<div class="crop-wrapper" style="left:150px;top:120px; width:80px; height:80px">
+		<div class="crop-vline"></div>
+		<div class="crop-vline right"></div>
+		<div class="crop-hline"></div>
+		<div class="crop-hline bottom"></div>
+	</div>
+</div>
+
 @stop
--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -1,7 +1,18 @@
 <html>
-    <body>
-        <h1>アノテーションツールつくるよ</h1>
-
-        @yield('content')
-    </body>
+	<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<script type="text/javascript" src="lib/jquery/js/jquery.min.js"></script>
+		<script type="text/javascript" src="lib/underscore/js/underscore.js"></script>
+		<script type="text/javascript" src="lib/gumby/js/gumby.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="lib/gumby/css/gumby.css" />
+		@yield('script')
+	</head>
+	<body>
+		<div class="row">
+			<div class="twelve columns">
+				<h1>アノテーションツールつくるよ</h1>
+				@yield('content')
+			</div>
+		</div>
+	</body>
 </html>
